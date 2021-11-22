90223398827fe73dabf7a6e69f76f3ce8a9214ef
Arthur L <arthur.louradou@centrale-marseille.fr>
2021-11-22 14:16:19 +0100

    Implemented animalerie

--- animalerie/models.py
+++ animalerie/models.py
@@ -2,7 +2,29 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
+
+class Equipement(models.Model):
+    id_equip = models.CharField(max_length=100, primary_key=True)
+    disponibilite = models.CharField(max_length=20)
+    photo = models.CharField(max_length=200)
+    def __str__(self):
+        return self.id_equip
+
+
+class Animal(models.Model):
+    id_animal = models.CharField(max_length=100, primary_key=True)
+    etat = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    race = models.CharField(max_length=20)
+    photo = models.CharField(max_length=200)
+    lieu = models.ForeignKey(Equipement, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.id_animal
+
+
+
+
 
 class Billet(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- animalerie/views.py
+++ animalerie/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Billet
+from .models import Animal, Billet, Equipement
 
 
 # Create your views here.
@@ -9,3 +9,15 @@ from .models import Billet
 def post_list(request):
     billets = Billet.objects.order_by('published_date')
     return render(request, 'animalerie/post_list.html', {'billets': billets})
+
+def animal_list(request):
+    animals = Animal.objects.all()
+    return render(request, 'animalerie/animal_list.html', {'animals': animals})
+
+def equipement_list(request):
+    equipements = Equipement.objects.all()
+    return render(request, 'animalerie/animal_list.html', {'animals': equipements})
+
+def animal_detail(request,store_id):
+    animal = Animal.objects.filter(id_animal=store_id)
+    return render(request, 'animalerie/animal_list.html', {'animal': animal})
